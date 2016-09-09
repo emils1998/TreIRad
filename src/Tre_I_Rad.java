5ec409c3e39b9d9df3ccf7cf25bc9fd1fd5c9544
Emil Schmidt <scem14@nti.umea>
2016-09-09 11:07:13 +0200

    Initial commit

---
+++ src/Tre_I_Rad.java
@@ -0,0 +1,32 @@
+/**
+ * Created by scem14 on 2016-09-09.
+ */
+public class Tre_I_Rad {
+
+    char x1;
+    char x2;
+    char x3;
+    char z4;
+    char z5;
+    char z6;
+    char w7;
+    char w8;
+    char w9;
+
+    public static void main(String[] arg) {
+        char x1 = '-';
+        char x2 = '-';
+        char x3 = '-';
+        char z4 = '-';
+        char z5 = '-';
+        char z6 = '-';
+        char w7 = '-';
+        char w8 = '-';
+        char w9 = '-';
+
+        System.out.println("_" + "x" + "|" + "z" + "|" + "w");
+        System.out.println("-" + "x" + "-" + "x" + "-" + "x");
+        System.out.println("-" + "x" + "-" + "x" + "-" + "x");
+        System.out.println("-" + "x" + "-" + "x" + "-" + "x");
+    }
+}
---
+++ src/asdg.java
@@ -0,0 +1,6 @@
+/**
+ * Created by scem14 on 2016-09-09.
+ */
+public class asdg {
+    public static void main(String[] arg)
+}
